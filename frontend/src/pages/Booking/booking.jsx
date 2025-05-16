62ac04bfaac693ea957ba8ac0c8f1be5c3b2b9c6
ermakgithub <152062275+ermakgithub@users.noreply.github.com>
2025-05-16 23:44:19 +0300

    Style Booking + RoomPage and Landing  start

--- frontend/src/pages/Booking/booking.jsx
+++ frontend/src/pages/Booking/booking.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import RoomsCard from '../../Components/Card/RoomsCard.jsx';
 import { getAvailableRooms } from '../../api/hotel-api.js';
 
-function Booking({ name,  type }) {
+function Booking({ name,  type, price, image }) {
     const [rooms, setRooms] = useState([]);
 
     useEffect(() => {
---
+++ frontend/src/pages/RoomPage/RoomPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function RoomPage() {
+    return (
+        <div></div>
+    );
+}
+
+export default RoomPage;
