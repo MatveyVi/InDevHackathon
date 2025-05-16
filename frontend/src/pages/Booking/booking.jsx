7ff795f5253535a1d3373a79c64dfebc5454759d
ermakgithub <152062275+ermakgithub@users.noreply.github.com>
2025-05-16 18:34:02 +0300

    Booking card

--- frontend/src/pages/Booking/booking.jsx
+++ frontend/src/pages/Booking/booking.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import RoomsCard from "../../Components/Card/RoomsCard.jsx";
 
-function Booking() {
+
+function Booking({name, status,temp, humidity}) {
+
+    const [rooms, setRooms] = useState([]);
+
+    useEffect(() => {
+        //setRooms
+    }, [])
+
     return (
-        <div></div>
+
+        <div className="h-full">
+        <div className='items-center justify-center flex mt-2 mb-5  border-spacing-1 h-full font-bold text-2xl' >
+            <p className="mt-1  ">Booking</p>
+        </div>
+       <RoomsCard/>
+        </div>
+
     );
 }
 
